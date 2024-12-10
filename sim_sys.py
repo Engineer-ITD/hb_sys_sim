b408a2686751b4f472be4e0fef46a96e65058c5e
Engineer-ITD <crazychestor@gmail.com>
2024-12-11 00:22:31 +0500

    Реализовано состояние серверов, бесконечный цикл с вводом заданий

--- sim_sys.py
+++ sim_sys.py
@@ -10,12 +10,40 @@ def server_init(nums, base):
         base[f'Сервер {i+1}'] = server_i
 
 
+def show_status_serv(base):
+    print('Состояние серверов:')
+    for serv in base:
+        if base[serv]['status'] == 'пусто':
+            print(f'{serv}: пусто')
+        else:
+            print(f'Выполняет задание (осталось {2+2} сек.)')
+
+
+def show_help():
+    print('Команды:\n'
+          '"добавить n" - для добавления задания, n - количество секунд.\n'
+          '"состояние серверов" - для перехода в режим обработки заданий\n'
+          '"выход" - для завершения работы')
+
+
 num_servers = int(input('Добро пожаловать в симулятор распределенной системы.\nВведите количество серверов: '))
 vir_sys = {}
 
 server_init(num_servers, vir_sys)
+show_status_serv(vir_sys)
+show_help()
+while True:
+    input_command = input('Команда: ').split()
+    if input_command[0] == 'добавить':
+        print(f'задание с {input_command[1]} секундами добавлено в очередь')
+    elif input_command == ['состояние', 'серверов']:
+        print()
+        show_status_serv(vir_sys)
+        print()
+    elif input_command[0] == 'выход':
+        print('\nЗавершение работы.')
+        break
 
-print(vir_sys)
 '''
 server_i = {'name': f'Сервер {i}',
                     'status': 'пусто',
